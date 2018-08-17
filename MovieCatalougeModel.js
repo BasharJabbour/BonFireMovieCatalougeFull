bf8d6fdc5e0c94a1a9e5c2ee7c71b7499c7678f5
Bashar Jabbour <basharjabbour@gmail.com>
2018-08-16 23:59:13 -0400

    flushed out knockout viewmodel

--- MovieCatalougeModel.js
+++ MovieCatalougeModel.js
@@ -1,10 +1,21 @@
 var movieCatalouge = function (htmlElement) {
+  ko.options.deferUpdates = true;
   var movieCatalougeViewModel = function () {
     var self = {};
     var url = 'localhost:8080';
     self.movies = ko.observableArray();
     self.addMovie = function () {};
     self.search = function () {};
+
+    return self;
+  };
+
+  var movieViewModel = function () {
+    var self = {};
+
+    self.title = ko.observable();
+    self.actors = ko.observableArray();
+    self.genres = ko.observableArray();
 
     return self;
   };
